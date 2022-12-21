f1700144806c07ed35554b5633acfb617eb4a431
arifmuhamadiqbal <arifmi2002@gmail.com>
2022-12-21 13:11:22 +0700

    add rooms frontnd

--- src/components/admin/FormTambah.js
+++ src/components/admin/FormTambah.js
@@ -4,45 +4,40 @@ import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 
 const FormTambah = () => {
-  /* const [kodeKamar, setKodeKamar] = useState("");
+  const [kodeKamar, setKodeKamar] = useState("");
   const [namaKamar, setNamaKamar] = useState("");
   const [deskripsiKamar, setDeskripsiKamar] = useState("");
   const [hargaKamar, setHargaKamar] = useState("");
-  const [namaFoto, setNamaFoto] = useState("");
-  const [urlFoto, setUrlFoto] = useState("");
-  const [file, setFile] = useState("");
-  const [preview, setPreview] = useState("");
+  const [namaFile, setNamaFile] = useState("");
+  const [file, setFile] = useState();
   const navigate = useNavigate();
 
-  console.log(kodeKamar);
-  console.log(namaKamar);
-  console.log(deskripsiKamar);
-  console.log(hargaKamar);
-  console.log(file);
-
-  const loadImage = (e) => {
+  /* const loadImage = (e) => {
     const image = e.target.files[0];
     setFile(image);
     setPreview(URL.createObjectURL(image));
-  };
+  }; */
 
-  const tambah = async (e) => {
+  /* const tambah = async (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("kodeKamar", kodeKamar);
     formData.append("namaKamar", namaKamar);
     formData.append("deskripsiKamar", deskripsiKamar);
     formData.append("hargaKamar", hargaKamar);
-    console.log("isi file adalah ", file);
+    formData.append("filename", namaFile);
+    formData.append("file", fileKamar);
     try {
-      await axios.post("http://localhost:3020/addRooms", formData);
+      await axios.post("http://localhost:3020/addRooms", formData)
+        .then((data) => {
+          console.log(data);
+        })
     } catch (error) {
       console.log(error.message);
     }
-  };
+  }; */
 
-  return ( */
-    {/*  {preview ? (
+  {/*  {preview ? (
               <img
                 style={{ width: "200px", height: "250px" }}
                 src={preview}
@@ -51,63 +46,79 @@ const FormTambah = () => {
             ) : (
               ""
             )} */}
-    {/* <div className="container mt-5">
+
+  return (
+    <div className="container mt-5">
       <div className="row">
         <div className="col-md-7">
-        <form className="space-y-6"
-                onSubmit={(event) => {
-                  var formdata = new FormData();
-
-                  formdata.append("nama", newProduct.nama);
-                  formdata.append("detail", newProduct.detail);
-                  formdata.append("kuantitas", newProduct.kuantitas);
-                  formdata.append("harga", newProduct.harga);
-                  formdata.append("id_jenis", newProduct.id_jenis);
-                  formdata.append("filename", newProduct.filename);
-                  formdata.append("file", newProductFile);
-                  axios
-                    .post(`http://localhost:4000/tambah-produk`, formdata)
-                    .then((data) => {
-                      console.log("gege");
-                      console.log(data);
-                    });
-                }}
-              />
-              <div>
-                  <label
-                    htmlFor="nama-produk"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Nama Produk
-                  </label>
-                  <input
-                    onChange={(event) => {
-                      let val = event.target.value;
-                      setNewProduct({
-                        nama: val,
-                        detail: newProduct.detail,
-                        kuantitas: newProduct.kuantitas,
-                        harga: newProduct.harga,
-                        id_jenis: newProduct.id_jenis,
-                        filename: newProduct.filename,
-                      });
-                    }}
-                    type="text"
-                    name="nama-produk"
-                    id="nama-produk"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    placeholder="apa kek yang keren"
-                    required
-                  />
-                </div>
-
-            <Button variant="primary" type="submit" className="d-block mt-2">
-              Submit
-            </Button>
+          <form className="space-y-6"
+            onSubmit={(event) => {
+              const formData = new FormData();
+              formData.append("kodeKamar", kodeKamar);
+              formData.append("namaKamar", namaKamar);
+              formData.append("deskripsiKamar", deskripsiKamar);
+              formData.append("hargaKamar", hargaKamar);
+              formData.append("filename", namaFile);
+              formData.append("file", file);
+              try {
+                axios.post("http://localhost:3020/addRoom", formData)
+                  .then((data) => {
+                    console.log(data);
+                  })
+              } catch (error) {
+                console.log(error.message);
+              }
+            }}
+          />
+          <div>
+            <label>Room Code</label>
+            <input
+            type="text"
+            name="kodeKamar" 
+            value={kodeKamar}
+              onChange={(e) => {
+                setKodeKamar(e.target.value);
+              }} />
+            <label>Room Name</label>
+            <input
+            type="text" 
+            name="namaKamar"
+            value={namaKamar}
+              onChange={(e) => {
+                setNamaKamar(e.target.value);
+              }} />
+            <label>Room Desciption</label>
+            <input
+            type="text"
+            name="deskripsiKamar"
+            value={deskripsiKamar}
+              onChange={(e) => {
+                setDeskripsiKamar(e.target.value);
+              }} />
+            <label>Room Price</label>
+            <input
+            type="number"
+            name="hargaKamar"
+            value={hargaKamar}
+              onChange={(e) => {
+                setHargaKamar(e.target.value);
+              }} />
+            <label>Rooom Photo</label>
+            <input
+            type="file"
+              onChange={(e) => {
+                const foto = e.target.files[0]
+                setFile(foto);
+                setNamaFile(foto.name);
+              }} />
+          </div>
+          <Button type="submit">
+            Submit
+          </Button>
         </div>
       </div>
-    </div> */}
- /*  ); */
+    </div>
+  );
 };
 
 export default FormTambah;
